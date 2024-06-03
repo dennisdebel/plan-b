ed6277cb83345c004243ab5994c8edbc1631062c
dennisdebel <dennisdebel@gmail.com>
2024-06-04 01:43:03 +0800

    fixed off by one error

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -18,8 +18,8 @@ const int PIN_A = D1;
 const int PIN_B = D2;
 const int PIN_C = D5;
 
-int buttonValue[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // debounce
-int lastButtonValue[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; // debounce
+int buttonValue[8] = {0,1,2,3,4,5,6,7}; // debounce (should be 8 zeros...)
+//int lastButtonValue[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; // debounce
 int b0 = 0; // channel storage
 int b1 = 0;
 int b2 = 0;
@@ -51,30 +51,37 @@ void muxOne() {
 
 for (int buttonCount = 0; buttonCount < 8; buttonCount++) {
 
-    Serial.print(" "); // DEBUG
-    Serial.print("Reading mux 1 pin: "); // DEBUG
-    Serial.println(buttonCount); // DEBUG
-    delay(1000); // DEBUG slow down for debuging
+
+
 
     b0 = bitRead(buttonCount,0); // convert buttonCount integer to bits and assign the first bit to the variable b0
     b1 = bitRead(buttonCount,1); // convert buttonCount integer to bits and assign the second bit to the variable b1
     b2 = bitRead(buttonCount,2); // convert buttonCount integer to bits and assign the last bit to the variable b2
 
-    Serial.print(" "); // DEBUG
-    Serial.print("Setting Channel: "); // DEBUG
-    Serial.print(bitRead(buttonCount,0)); // DEBUG
-    Serial.print(bitRead(buttonCount,1)); // DEBUG
-    Serial.print(bitRead(buttonCount,2)); // DEBUG
+
 
     digitalWrite(PIN_A,b0);
     digitalWrite(PIN_B,b1);
     digitalWrite(PIN_C,b2);
-    buttonValue[buttonCount] = digitalRead(PIN_VALUE_ONE);
 
-    Serial.print(" "); // DEBUG
-    Serial.print("Value: "); // DEBUG
-    Serial.print(buttonValue[buttonCount] = digitalRead(PIN_VALUE_ONE)); // DEBUG
+
+
   
+    //Serial.print(" "); // DEBUG
+   // Serial.print("Reading mux 1 pin: "); // DEBUG
+    //Serial.println(buttonCount); // DEBUG
+    //Serial.print(" "); // DEBUG
+    // Serial.print("Setting Channel: "); // DEBUG
+    // Serial.print(bitRead(buttonCount,0)); // DEBUG
+    // Serial.print(bitRead(buttonCount,1)); // DEBUG
+    // Serial.print(bitRead(buttonCount,2)); // DEBUG
+    //Serial.print(" "); // DEBUG
+    Serial.print("Value: "); // DEBUG
+    Serial.print(" "); // DEBUG
+    Serial.print(buttonValue[buttonCount]); // DEBUG, this gets called for the second time, so...numbers are off ?
+    Serial.print(" "); // DEBUG
+    Serial.println(digitalRead(PIN_VALUE_ONE)); // DEBUG, this gets called for the second time, so...numbers are off ?
+    delay(1000); // DEBUG slow down for debuging
 
 
     // if(buttonValue[buttonCount] == LOW && lastButtonValue[buttonCount] == HIGH) {
